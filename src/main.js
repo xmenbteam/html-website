75320ac89c3305fe6bd8fa0a2104f8dd0d075c2b
Sam Parry <samparrymusic@gmail.com>
2021-01-12 23:41:00 +0000

    makes website up to date for first hosting push

---
+++ src/main.js
@@ -0,0 +1,8 @@
+function validation() {
+
+    let name = document.getElementById('nameInput').value;
+    let email = document.getElementById('emailInput').value;
+    let message = document.getElementById('message').value;
+
+
+}
